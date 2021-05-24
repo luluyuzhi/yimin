ba05d4ec09ef44dcd5dd429bbe96984b3faa5086
muduo <aoumeior@outlook.com>
2021-05-24 22:35:53 +0800

    1

--- pre-install-slidev.sh
+++ pre-install-slidev.sh
@@ -1,10 +1,19 @@
 #!/bin/sh -l
 
 
-for TARGET in ./slidev/*
+npm i -g yarn
+
+yarn
+
+
+# yarn workspaces run build
+
+for TARGET in ./packages/*-slidev
 do
     if test -d $TARGET
     then
-        echo -e "[${TARGET##*/}](./slidev/${TARGET##*/}/index.html)\r\n" >> source/Works/index.md
+    echo  $TARGET
+        yarn run slidev build $TARGET/slides.md 
+        # echo -e "[${TARGET##*/}](./slidev/${TARGET##*/}/index.html)\r\n" >> source/Works/index.md
     fi
 done
